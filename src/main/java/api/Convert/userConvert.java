3b7910a026927f52571ea07dc6914c0c2ea31703
weedcry <44254503+weedcry@users.noreply.github.com>
2021-04-18 16:18:10 +0700

    Update userConvert.java

--- src/main/java/api/Convert/userConvert.java
+++ src/main/java/api/Convert/userConvert.java
@@ -19,7 +19,20 @@ public class userConvert {
 		udto.setPhone(u.getPhone());
 		udto.setPhoto(u.getPhoto());
 		udto.setBirthday(u.getBirthday());
-		udto.setDate_create(u.getDate_create());	
+		udto.setDate_create(u.getDate_create());
+		udto.setActive(u.getActive());
+		return udto;
+	}
+	
+	public userDTO touserDTO_channel(user u) {
+		userDTO udto = new userDTO();
+		udto.setId(u.getId());
+		udto.setName(u.getName());
+		udto.setPassword(u.getPassword());
+		udto.setPhone(u.getPhone());
+		udto.setPhoto(u.getPhoto());
+		udto.setBirthday(u.getBirthday());
+		udto.setDate_create(u.getDate_create());
 		udto.setActive(u.getActive());
 		return udto;
 	}
