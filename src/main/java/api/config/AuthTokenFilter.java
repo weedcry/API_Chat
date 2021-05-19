18f73125dd118c2b325ace7171d53f058f3fd889
weedcry <44254503+weedcry@users.noreply.github.com>
2021-05-19 14:56:28 +0700

    Update AuthTokenFilter.java

--- src/main/java/api/config/AuthTokenFilter.java
+++ src/main/java/api/config/AuthTokenFilter.java
@@ -54,7 +54,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 
     private String parseJwt(HttpServletRequest request){
         String headerAuth = request.getHeader("Authorization");
-        if(StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")){
+        if(StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer_")){
             return  headerAuth.substring(7,headerAuth.length());
         }
 
