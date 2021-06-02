dcb9517171b237f43766155dc41fa6706e3e9d3c
weedcry <44254503+weedcry@users.noreply.github.com>
2021-06-02 17:57:47 +0700

    Update channelService.java

--- src/main/java/api/service/channelService.java
+++ src/main/java/api/service/channelService.java
@@ -87,9 +87,7 @@ public class channelService {
 		channel c = channelRes.findchannelbyfriend(username,friendid);
 		if(c == null){
 			result.setMessage("not found");
-			return ResponseEntity
-					.ok()
-					.body(new MessageResponse("not found!"));
+			return 0;
 		}
 		return c;
 	}
