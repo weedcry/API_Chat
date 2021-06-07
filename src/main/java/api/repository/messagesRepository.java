9f824c066512f90d0e5426d97740c5796ecb0116
weedcry <44254503+weedcry@users.noreply.github.com>
2021-06-07 16:11:17 +0700

    Update messagesRepository.java

--- src/main/java/api/repository/messagesRepository.java
+++ src/main/java/api/repository/messagesRepository.java
@@ -11,7 +11,7 @@ public interface messagesRepository extends JpaRepository<messages,Long> {
 	@Query(value = "SELECT m.* FROM messages as m WHERE m.id = ?1 and m.channel_id = ?2",nativeQuery = true)
 	messages findById(Long id,Long channel_id);
 	
-	@Query(value = "SELECT m.* FROM messages as m WHERE m.channel_id = ?1",nativeQuery = true)
+	@Query(value = "SELECT m.* FROM messages as m WHERE m.channel_id = ?1 and status = 1",nativeQuery = true)
 	List<messages> findByChannel_General(Long channel_general);
 
 	@Query(value = "SELECT m.* FROM messages as m WHERE m.channel_id = ?1 and m.id = (SELECT MAX(m.id) FROM messages as m where m.channel_id = ?1)",nativeQuery = true)
@@ -19,7 +19,7 @@ public interface messagesRepository extends JpaRepository<messages,Long> {
 
 	@Query(value = "select * from messages where id in(SELECT max(m.id) " +
 			"FROM (select * from messages where channel_id in (select id from channel where author_id = ?1)) as m  " +
-			"group by m.channel_id) ORDER BY channel_id;",nativeQuery = true)
+			"group by m.channel_id) and status = 1 ORDER BY channel_id;",nativeQuery = true)
 	List<messages> findLastMessageByUserid(String userid);
 
 	
