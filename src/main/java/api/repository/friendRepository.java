170613cf51d1fec28cc5de98484d6fd244c97a7a
weedcry <44254503+weedcry@users.noreply.github.com>
2021-06-10 11:44:09 +0700

    Add files via upload

--- src/main/java/api/repository/friendRepository.java
+++ src/main/java/api/repository/friendRepository.java
@@ -12,4 +12,6 @@ public interface friendRepository  extends JpaRepository<friend,String> {
     @Query(value = "SELECT DISTINCT * FROM friend WHERE id = ?1",nativeQuery = true)
     List<friend> findById(String id);
 
+    @Query(value = "SELECT * FROM friend WHERE id = ?1 and friend_id = ?2",nativeQuery = true)
+    friend findfriend(String id, String userid);
 }
--- src/main/java/api/service/friendService.java
+++ src/main/java/api/service/friendService.java
@@ -62,7 +62,7 @@ public class friendService {
     public Object acceptFriend(String username,friendDTO fr1){
         ServiceResult result = new ServiceResult();
         user u =  userConvert.touser((userDTO) userService.findById(username));
-        friendDTO fr = new friendDTO(fr1.getId(),u,1,u.getActive());
+        friendDTO fr = new friendDTO(fr1.getFriend().getId(),u,1,u.getActive());
         fr1.setFriend_active(1);
         try {
             friendRes.save(friendConvert.tofriend(fr));
