40b51b0488aa31b7ac4bce9c91c5d14a8812999b
weedcry <44254503+weedcry@users.noreply.github.com>
2021-07-11 14:17:51 +0700

    Update channelRepository.java

--- src/main/java/api/repository/channelRepository.java
+++ src/main/java/api/repository/channelRepository.java
@@ -20,7 +20,7 @@ public interface channelRepository extends JpaRepository<channel,Long> {
 	@Query(value = "SELECT c.* FROM channel as c where c.id = ?1 and c.author_id = ?2", nativeQuery = true)
 	channel findOneByAuthorid(Long id ,String author_id);
 
-	@Query(value = "select * from channel where author_id = ?2 and id in (select id from channel where author_id = ?1) and exits = 1 and status = 1  ;", nativeQuery = true)
+	@Query(value = "select * from channel where author_id = ?1 and id in (select id from channel where author_id = ?2) and exits = 1 and status = 1  ;", nativeQuery = true)
 	channel findchannelbyfriend(String username,String friendid);
 
 }
